e8982230f22d5aa6485dfa77da2fbc273bbdebb7
Brian Adriance <adriab00100@gmail.com>
2020-06-01 09:34:21 -0400

    initial commit

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>PAGE NOT FOUND</h1>
+    <p>There's nothing here.</p>
+  </Layout>
+)
+
+export default NotFoundPage
