9a574f21dfbf5234878c3bf843e735b96d094ec6
Brian Adriance <adriab00100@gmail.com>
2020-06-22 14:04:23 -0400

    Added 404 page and some footer styles and content

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Image } from 'cloudinary-react';
+import { Link } from "gatsby"
+import { Image, Transformation } from 'cloudinary-react';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { cloudinaryCloudId } from "../images/constants"
@@ -7,13 +8,20 @@ import { cloudinaryCloudId } from "../images/constants"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>PAGE NOT FOUND</h1>
-    <p>There's nothing here.</p>
-
-
-    <Image cloudName={cloudinaryCloudId} publicId="notFound_ipwmwf" width="440" crop="scale" />
-
-
+    <div className="centering-container">
+      <h1>PAGE NOT FOUND</h1>
+    </div>
+    <div className="centering-container">
+      <div className="side-by-side-container">
+        <Image cloudName={cloudinaryCloudId} secure="true" width="440" alt="An empty tool bag" publicId="site-assets/notFound_ipwmwf" >
+          <Transformation width="440" crop="scale" fetchFormat="auto" quality="auto" />
+        </Image>
+        <div className="stacked-container pls" >
+          <h2>There's nothing here</h2>
+          <span>Unfortunately the page you're trying to visit doesn't seem to exist.<br />Try going back to the <Link to="/">home page</Link> instead.</span>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
