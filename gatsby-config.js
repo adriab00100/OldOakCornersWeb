5a62f964528b035f627da328cf7f1f2d74710bb3
Brian Adriance <adriab00100@gmail.com>
2020-07-24 21:54:35 -0400

    1. Rss feeds 2. Sitemap 3. Gatsby version bump and vuln fixes 4. Post from the past about corner shelf

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Old Oak Corners`,
     description: `A blog where Brian shares wood-working thoughts, ideas, and projects.`,
     author: `Brian Adriance`,
+    siteUrl: `https://www.brianadriance.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -68,5 +69,59 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        path
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Old Oak Corners' RSS Feed",
+          },
+        ]
+      }
+    },
+    `gatsby-plugin-sitemap`
   ],
 };
